c27a3ed8ea2f25d19852df21a7a59aaaa5f55c8f
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-24 15:30:45 +0200

    Restructure temp display

--- src/render-weather.js
+++ src/render-weather.js
@@ -12,19 +12,23 @@ function renderWeather() {
   humidityDiv.replaceChildren();
   precipDiv.replaceChildren();
   const locationDisplay = document.createElement('p');
+  const tempDiv = document.createElement('div');
   const tempDisplay = document.createElement('p');
   const conditionImg = document.createElement('img');
   const conditionDisplay = document.createElement('p');
   const feelsLike = document.createElement('p');
+  const degree = document.createElement('p');
   const humidityTitle = document.createElement('p');
   const humidityDisplay = document.createElement('p');
   const precipTitle = document.createElement('p');
   const precipDisplay = document.createElement('p');
 
+  tempDiv.classList.add('temp-div');
   locationDisplay.classList.add('location-title');
   tempDisplay.classList.add('temp-display');
   conditionDisplay.classList.add('condition-display');
   feelsLike.classList.add('feels-like');
+  degree.classList.add('degrees');
   humidityTitle.classList.add('humidity-title');
   humidityDisplay.classList.add('humidity-display');
   precipTitle.classList.add('precipitation-title');
@@ -37,15 +41,16 @@ function renderWeather() {
   humidityDisplay.textContent = `${weatherArr[0].humidity}%`
   conditionImg.classList.add('condition-img')
   conditionImg.src = `http://${weatherArr[0].conditionIcon}`;
+  degree.textContent = `\u00B0`;
 
   if (!isCelcius) {
-    tempDisplay.textContent = `${weatherArr[0].tempF}\u00B0 F`;
-    feelsLike.textContent = `Feels like ${weatherArr[0].feelsLikeF}\u00B0 F`;
+    tempDisplay.textContent = `${weatherArr[0].tempF}`;
+    feelsLike.textContent = `Feels like ${weatherArr[0].feelsLikeF}\u00B0`;
     precipTitle.textContent = 'Precipitation';
     precipDisplay.textContent = `${weatherArr[0].precipIn} in.`;
   } else {
-    tempDisplay.textContent = `${weatherArr[0].tempC}\u00B0 C`;
-    feelsLike.textContent = `Feels like ${weatherArr[0].feelsLikeC}\u00B0 C`;
+    tempDisplay.textContent = `${weatherArr[0].tempC}`;
+    feelsLike.textContent = `Feels like ${weatherArr[0].feelsLikeC}\u00B0`;
     precipTitle.textContent = 'Precipitation';
     precipDisplay.textContent = `${weatherArr[0].precipMM} mm`;
   }
@@ -57,7 +62,9 @@ function renderWeather() {
   }
 
   weatherDiv.appendChild(locationDisplay);
-  weatherDiv.appendChild(tempDisplay);
+  weatherDiv.appendChild(tempDiv);
+  tempDiv.appendChild(tempDisplay);
+  tempDiv.appendChild(degree);
   weatherDiv.appendChild(conditionDisplay);
   weatherDiv.appendChild(conditionImg);
   weatherDiv.appendChild(feelsLike);
