0f9a9e96797c60b3a3a49467f04d41a11c770b32
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-24 08:01:06 +0200

    Add function to search button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import getWeather from './get-weather';
 import './style.css';
+
+const input = document.querySelector('.location');
+const searchBtn = document.querySelector('.search');
+
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const locationInput = input.value;
+  getWeather(locationInput);
+});
