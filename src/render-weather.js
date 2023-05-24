a650f817b7bcd931aebb549accaa70005cc9f914
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-24 11:18:50 +0200

    Create celcius to farenheit converter button

--- src/render-weather.js
+++ src/render-weather.js
@@ -3,29 +3,52 @@ import { weatherArr } from "./weather-factory";
 const weatherDiv = document.querySelector('.weather-info');
 const humidityDiv = document.querySelector('.humidity-info');
 const precipDiv = document.querySelector('.percipitation-info');
+const toggleCelciusBtn = document.querySelector('.toggle-cf');
+let isCelcius = false;
 
 function renderWeather() {
   weatherDiv.replaceChildren();
   humidityDiv.replaceChildren();
   precipDiv.replaceChildren();
   const locationDisplay = document.createElement('p');
+  const tempDisplay = document.createElement('p');
+  const conditionDisplay = document.createElement('p');
+  const feelsLike = document.createElement('p');
+  const humidityDisplay = document.createElement('p');
+  const precipDisplay = document.createElement('p');
+
+  if (!isCelcius) {
+    tempDisplay.textContent = `${weatherArr[0].tempF}\u00B0 F`;
+    feelsLike.textContent = `Feels like ${weatherArr[0].feelsLikeF}\u00B0 F`;
+    precipDisplay.textContent = `Precipitation ${weatherArr[0].precipIn} in.`;
+  } else {
+    tempDisplay.textContent = `${weatherArr[0].tempC}\u00B0 C`;
+    feelsLike.textContent = `Feels like ${weatherArr[0].feelsLikeC}\u00B0 C`;
+    precipDisplay.textContent = `Precipitation ${weatherArr[0].precipMM} mm`;
+  }
+
   locationDisplay.textContent = `${weatherArr[0].location}`;
   weatherDiv.appendChild(locationDisplay);
-  const tempDisplay = document.createElement('p');
-  tempDisplay.textContent = `${weatherArr[0].tempF}\u00B0 F`;
   weatherDiv.appendChild(tempDisplay);
-  const conditionDisplay = document.createElement('p');
   conditionDisplay.textContent = `${weatherArr[0].condition}`
   weatherDiv.appendChild(conditionDisplay);
-  const feelsLike = document.createElement('p');
-  feelsLike.textContent = `Feels like ${weatherArr[0].feelsLikeF}\u00B0 F`;
   weatherDiv.appendChild(feelsLike);
-  const humidityDisplay = document.createElement('p');
   humidityDisplay.textContent = `Humidity ${weatherArr[0].humidity}%`;
   humidityDiv.appendChild(humidityDisplay);
-  const precipDisplay = document.createElement('p');
-  precipDisplay.textContent = `Precipitation ${weatherArr[0].precipIn} in.`;
   precipDiv.appendChild(precipDisplay);
 }
+
+function changeCelcius() { 
+  if(!isCelcius) {
+    isCelcius = true;
+    renderWeather();
+  } else {
+    isCelcius = false;
+    renderWeather();
+  }
+}
+
+toggleCelciusBtn.addEventListener('click', changeCelcius);
+
 
 export default renderWeather;
